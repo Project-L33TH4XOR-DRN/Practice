6b934141934152ab02ee5faf02376baf696cd0d4
Jeremy Fong <Jeremy_fong5@hotmail.com>
2017-03-06 02:45:40 -0500

    Add some code for floor and test.cpp

--- floor.cpp
+++ floor.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
 
+width = 10;
+
+
+void Draw()
+{
+
+	system("cls"); //clear terminal screen
+	for (int i = 0; i < width; i++)
+		cout << "_";
+	cout << endl;
+
+}
+
+
 
 int main() 
 {
-	cout >> "Are we making a 2D side scroller or a 2D view from above?"
+
 
 
 }
