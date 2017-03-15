a438d64187e8474edb0a72a0c9d18cdd40d428e3
VicentiuCristianBadea <vily.badea@gmail.com>
2017-03-15 16:11:27 -0400

    commiting character/main maybe

--- floor.cpp
+++ floor.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 using namespace std;
 
-<<<<<<< HEAD
-int width = 30;
 
-=======
 int width = 50;
->>>>>>> 11617ed6b573ff6aea1572e351cb7b288fd537b3
+
 
 void Draw()
 {
@@ -15,13 +12,13 @@ void Draw()
 	cout << "\n\n\n\n\n";
 	for (int i = 0; i < width; i++)
 		cout << "_";
-<<<<<<< HEAD
+
 	cout << endl;
 	for(int j=0; j < width; j++)
 		cout << "|";
-=======
+
 	cout << endl << endl;
->>>>>>> 11617ed6b573ff6aea1572e351cb7b288fd537b3
+
 
 }
 
